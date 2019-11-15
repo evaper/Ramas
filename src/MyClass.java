f624c4ffa1d32bb185cfcf3659c69dd5bcaa771e
Eva Peralta <eva.peralta@gmail.com>
2019-11-15 14:28:45 +0100

    He añadido la variable c
    Vble c

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,11 +3,13 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		
-		int a,b;
+		int a,b,c;
 		a=5;
 		System.out.println(a);
 		b=10;
 		System.out.println(b);
+		c=9;
+		System.out.println(c);
 
 	}
 
