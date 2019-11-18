d9685fd6a0987f13d75c24c747867f756dd4fcf2
Eva Peralta <eva.peralta@gmail.com>
2019-11-18 08:49:07 +0100

    He añadido la variable d
    Vble d añadida

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,13 +3,15 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		
-		int a,b,c;
+		int a,b,c,d;
 		a=5;
 		System.out.println(a);
 		b=10;
 		System.out.println(b);
 		c=9;
 		System.out.println(c);
+		d=15;
+		System.out.println(d);
 
 	}
 
